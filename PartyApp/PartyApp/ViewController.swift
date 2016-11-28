75015664bb3c8d1f437a74866334cf6724af4cbc
ylf951 <ylf951@gmail.com>
2016-11-27 22:02:17 -0500

    Initial commit

---
+++ PartyApp/PartyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PartyApp
+//
+//  Created by Linfeng Yu on 16/11/27.
+//  Copyright © 2016年 Linfeng Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
